ffe1cacc95414b83e415b7425ff12e7db8bf65cc
jphy1314 <41810119+jphy1314@users.noreply.github.com>
2026-02-28 11:06:45 +0800

    Modify DIY script for Passwall source management
    Updated DIY script to remove old Passwall references and add new Passwall sources.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# DIY Part 1: 基础设置
-# 不拉任何 helloworld / Rust feed
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 修改默认 IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# 1. 移除可能存在的冲突源 (清理 Lean 源码中可能自带的旧 Passwall 引用)
+sed -i '/passwall/d' feeds.conf.default
+sed -i '/helloworld/d' feeds.conf.default
+
+# 2. 添加 PassWall 源 (必须两个都加：packages 是依赖，passwall 是主程序)
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
